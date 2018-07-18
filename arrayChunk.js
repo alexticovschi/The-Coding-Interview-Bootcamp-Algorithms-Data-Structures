0d924be2ca45f48faa33ad73bf974a7ecbaec961
AlexTicovschi <alexmticovschi@gmail.com>
2018-07-18 21:55:13 +0100

    array chunk - solution #2

--- arrayChunk.js
+++ arrayChunk.js
@@ -8,6 +8,7 @@
 // chunk([1 ,2, 3, 4], 10) --> [[1, 2, 3, 4, 5]]
 
 
+/*** First Solution ***/ 
 const chunk = (array, size) => {
     // an empty array that holds chunks
     let chunks = [];
@@ -16,6 +17,30 @@ const chunk = (array, size) => {
         chunks.push(array.slice(index, index + size));
     }
     return chunks;
+}
+
+/*** Second Solution ***/ 
+/***
+ * #1 Create empty array to hold chunks called 'chunked'
+ * #2 For each element in the *unchunked* array
+ *      Retrieve the last element in 'chunked'
+ *      If last element does not exist, or if its length is equal to chunk size
+ *          Push a new chunk into 'chunked' with the current element
+ *      Else add the current element into the chunk
+ ***/ 
+const chunk = (array, size) => {
+    const chunked = [];
+
+    for(let element of array) {
+        const last = chunked[chunked.length - 1];
+
+        if(!last || last.length === size) {
+            chunked.push([element]);
+        } else {
+            last.push(element);
+        }
+    }
+    return chunked;
 }
 
 chunk([1,2,3,4,5,6,7,8], 2);
