a5810ca6b1764d2a288d60c28fe5857d94f18d51
AlexTicovschi <alexmticovschi@gmail.com>
2018-07-26 18:41:45 +0100

    printing steps - solution #2

--- steps.js
+++ steps.js
@@ -16,6 +16,7 @@
 //       '### ' 
 //       '####' 
 
+// *** First Solution *** //
 const steps = (n) => {
     let steps = '';
     for(let i = 1; i < n; i++) {
@@ -24,4 +25,29 @@ const steps = (n) => {
     }
 }
   
+// *** Second Solution *** //
+// #1 From 0 to n iterate through rows
+// #2 create an empty string steps
+// #3 From 0 to n iterate through columns
+// #4 IF the current column is equal to to or less than the current row
+//      Add a '#' to 'steps'
+// #5 ELSE add a space to 'steps'
+// #6 Console log 'steps'       
+const steps = (n) => {
+    for(let row = 0; row < n; row++) {
+        let steps = '';
+        console.log('row',row);
+
+        for(let column = 0; column < n; column++) {
+            console.log('column',column);
+            if(column <= row) { 
+                steps += "#";
+            } else {
+                steps += ' ';
+            }
+        }
+        console.log(steps);
+    }
+}
+
 steps(4);
