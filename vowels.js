dbb8a4ca0ca00d3bf8675a447f2cabdbfd995e7a
AlexTicovschi <alexmticovschi@gmail.com>
2018-07-29 13:31:20 +0100

    finding vowels - solution #2

--- vowels.js
+++ vowels.js
@@ -5,6 +5,7 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why') --> 0
 
+/*** First Solution ***/ 
 const vowels = (str) => {
     // '/[^\w\s]/gi' - regex to replace unwanted characters from the string
     str = str.replace(/[^\w\s]/gi, '').toLowerCase();
@@ -13,6 +14,18 @@ const vowels = (str) => {
 
     for(let i = 0; i < str.length; i++) {
         vowels.match(str[i]) && counter++;
+    }
+    
+    return counter;
+}
+
+/*** Second Solution ***/ 
+const vowels = (str) => {
+    const vowels = ['a','e','i','o','u'];
+    let counter = 0;
+
+    for(let char of str.toLowerCase()) {
+        vowels.includes(char) && counter++;
     }
     
     return counter;
