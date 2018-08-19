4111b2b800e66f2a89f8dbf21f4e54d4f4a91765
AlexTicovschi <alexmticovschi@gmail.com>
2018-08-19 12:22:31 +0100

    pyramid - solution #1

---
+++ pyramid.js
@@ -0,0 +1,41 @@
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+//  with N levels using the # character. Make sure the
+//  pyramid has spaces on both left and right hand sides
+// --- Examples
+//   pyramid(1)
+//      '#'
+//   pyramid(2)
+//      ' # '
+//      '###'
+//   pyramid(3)
+//      '  #  '
+//      ' ### '
+//      '#####'
+
+// #1 From 0 to n (iterate through rows)
+// #2  Create an empty string, 'level'
+// #3  From 0 to ??? (columns)
+// #4 IF the column should have a #
+// #    Add a '#' to 'level'
+// #  ELSE
+// #    Add a space to 'level'
+// #5 Console log 'stair'
+
+const pyramid = (n) => {
+    const midpoint = Math.floor((2 * n - 1) / 2);
+
+    for(let row = 0; row < n; row++) {
+        let level = '';
+        for (let column = 0; column < 2 * n - 1; column++) {
+            if(midpoint - row <= column && midpoint + row >= column) { 
+                level += "#";
+            } else {
+                level += ' ';
+            }
+        }
+        console.log(level);
+    }
+}
+
+pyramid(3);
